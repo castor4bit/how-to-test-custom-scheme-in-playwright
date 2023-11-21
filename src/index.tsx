5d53ec16d5777188ab6c774cd0caad7cd4271396
castor4bit <castor.4bit@gmail.com>
2023-11-21 19:22:09 +0900

    Simplified HTML

--- src/index.tsx
+++ src/index.tsx
@@ -1,40 +1,23 @@
 import { serve } from "@hono/node-server";
 import { Hono } from "hono";
 import { html } from "hono/html";
-import type { FC } from "hono/jsx";
 
 const app = new Hono();
 
 const redirectUrl = "example://foo/bar?baz=xxx";
-const Layout = (props: { children?: any }) => html`
+const indexHtml = html`
   <!DOCTYPE html>
   <html>
     <body>
       <h1>Link to Custom Scheme</h1>
-      <div>${props.children}</div>
+      <div>
+        <a href="${redirectUrl}" data-testid="redirect-link">${redirectUrl}</a>
+      </div>
     </body>
   </html>
 `;
-const RedirectLink: FC<{ redirectUrl: string; testId: string }> = (props: {
-  redirectUrl: string;
-  testId: string;
-}) => (
-  <a href={props.redirectUrl} data-testid={props.testId}>
-    {props.redirectUrl}
-  </a>
-);
-const IndexPage: FC<{ redirectUrl: string }> = (props: {
-  redirectUrl: string;
-}) => (
-  <Layout>
-    <h1>Link to Custom Scheme</h1>
-    <div>
-      <RedirectLink redirectUrl={props.redirectUrl} testId="redirect-link" />
-    </div>
-  </Layout>
-);
 
-app.get("/", (c) => c.html(<IndexPage redirectUrl={redirectUrl} />));
+app.get("/", (c) => c.html(indexHtml));
 app.get("/redirect", (c) => c.redirect(redirectUrl));
 app.get("/redirect-twice", (c) => c.redirect("/redirect"));
 
