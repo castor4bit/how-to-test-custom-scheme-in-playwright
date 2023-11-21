88c41e46d44de1eb8fe850656d593d465622c53b
castor4bit <castor.4bit@gmail.com>
2023-11-21 19:07:59 +0900

    Would it be better to split the components?

--- src/index.tsx
+++ src/index.tsx
@@ -1,26 +1,40 @@
 import { serve } from "@hono/node-server";
 import { Hono } from "hono";
-import { FC } from "hono/jsx";
+import { html } from "hono/html";
+import type { FC } from "hono/jsx";
 
 const app = new Hono();
 
 const redirectUrl = "example://foo/bar?baz=xxx";
-const IndexPage: FC = () => {
-  return (
-    <html>
-      <body>
-        <h1>Link to Custom Scheme</h1>
-        <div>
-          <a href={redirectUrl} data-testid="redirect-link">
-            {redirectUrl}
-          </a>
-        </div>
-      </body>
-    </html>
-  );
-};
+const Layout = (props: { children?: any }) => html`
+  <!DOCTYPE html>
+  <html>
+    <body>
+      <h1>Link to Custom Scheme</h1>
+      <div>${props.children}</div>
+    </body>
+  </html>
+`;
+const RedirectLink: FC<{ redirectUrl: string; testId: string }> = (props: {
+  redirectUrl: string;
+  testId: string;
+}) => (
+  <a href={props.redirectUrl} data-testid={props.testId}>
+    {props.redirectUrl}
+  </a>
+);
+const IndexPage: FC<{ redirectUrl: string }> = (props: {
+  redirectUrl: string;
+}) => (
+  <Layout>
+    <h1>Link to Custom Scheme</h1>
+    <div>
+      <RedirectLink redirectUrl={props.redirectUrl} testId="redirect-link" />
+    </div>
+  </Layout>
+);
 
-app.get("/", (c) => c.html(<IndexPage />));
+app.get("/", (c) => c.html(<IndexPage redirectUrl={redirectUrl} />));
 app.get("/redirect", (c) => c.redirect(redirectUrl));
 app.get("/redirect-twice", (c) => c.redirect("/redirect"));
 
